dcbdfe4021f42c124a878a2ee543b27ebd5981a8
wevioz <wevioz@mail.ru>
2023-06-15 10:14:48 +0300

    Refactored Security config and created Application config

--- src/main/ru/wevioz/social_network/config/SecurityConfig.java
+++ src/main/ru/wevioz/social_network/config/SecurityConfig.java
@@ -1,59 +1,37 @@
 package wevioz.social_network.config;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import wevioz.social_network.service.UserService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import wevioz.social_network.filter.JwtCsrfFilter;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SecurityConfig {
-    private UserService service;
+    private final JwtCsrfFilter jwtCsrfFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public PasswordEncoder devPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    protected void configure(HttpSecurity http) throws Exception {
-
-    }
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder bCryptPasswordEncoder) {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user")
-                .password(bCryptPasswordEncoder.encode("userPass"))
-                .roles("USER")
-                .build());
-        manager.createUser(User.withUsername("admin")
-                .password(bCryptPasswordEncoder.encode("adminPass"))
-                .roles("USER", "ADMIN")
-                .build());
-        return manager;
-    }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeRequests(
-                        authorizeRequests -> authorizeRequests.requestMatchers("/users").hasRole("User")
-                ).jee(jee -> jee.mappableRoles("USER", "ADMIN"));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(req -> req
+                    .requestMatchers("")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+            ).sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            ).authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtCsrfFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(this.service);
     }
 }
